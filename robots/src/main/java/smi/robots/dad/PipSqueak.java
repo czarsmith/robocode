9c4d60f14cd69e2ca3e48e708e52222501400594
Dad <Dad@LAPTOP-2TB8RECV.domain>
2019-01-17 17:49:37 -0600

    PipSqueak

---
+++ robots/src/main/java/smi/robots/dad/PipSqueak.java
@@ -0,0 +1,34 @@
+package smi.robots.dad;
+
+import robocode.HitWallEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
+
+public class PipSqueak extends Robot {
+  private double dir = 1;
+  private double gunDir = 1;
+  private double radarLockAngle = 60;
+  
+  public void run() {
+    setAdjustGunForRobotTurn(true);
+    
+    turnGunRight(370);
+
+    while (true) {
+      ahead(50 * dir);
+      turnGunRight(gunDir * radarLockAngle * 2);
+    }
+  }
+
+  public void onHitWall(HitWallEvent e) { dir *= -1; }
+
+  public void onScannedRobot(ScannedRobotEvent e) {
+    if (getGunHeat() == 0) {
+      fire(3);
+    }
+    turnGunRight(gunDir * radarLockAngle);
+    gunDir *= -1;
+    turnRight(Utils.normalRelativeAngleDegrees(e.getBearing() - 90 + 20 * dir));
+  }
+}
